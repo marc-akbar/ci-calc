8c31c182c0bc26d0bb590e0a5ba94847e2e14e69
marc <marc.akbar@gmail.com>
2020-02-09 19:33:59 -0700

    Calculate annual growth

--- src/components/user-input.js
+++ src/components/user-input.js
@@ -16,12 +16,6 @@ class UserInput extends Component {
       testFunction,
       updateInputs,
       calculateMonthlyContributions,
-      current_age,
-      retirement_age,
-      stop_contribution_age,
-      current_savings,
-      percent_annual_return,
-      percent_to_contribute,
       monthly_contribution
     } = this.props;
 
@@ -29,28 +23,33 @@ class UserInput extends Component {
       <div>
         <div className='user-input'>
           Current Age
-          <input placeholder={current_age} onChange={ (e) => updateInputs(e.target.value, 'current_age') }></input>
+          <input onChange={ (e) => updateInputs(e.target.value, 'current_age') }></input>
         </div>
         <div className='user-input'>
           Age you plan on retiring
-          <input placeholder={retirement_age} onChange={ (e) => updateInputs(e.target.value, 'retirement_age') }></input>
+          <input onChange={ (e) => updateInputs(e.target.value, 'retirement_age') }></input>
         </div>
         <div className='user-input'>
           Age you plan to stop making contributions
-          <input placeholder={stop_contribution_age} onChange={ (e) => updateInputs(e.target.value, 'stop_contribution_age') }></input>
+          <input onChange={ (e) => updateInputs(e.target.value, 'stop_contribution_age') }></input>
         </div>
         <div className='user-input'>
           Current Retirement Savings
-          <input placeholder={current_savings} onChange={ (e) => updateInputs(e.target.value, 'current_savings') }></input>
+          <input onChange={ (e) => updateInputs(e.target.value, 'current_savings') }></input>
         </div>
         <div className='user-input'>
-          Percentage annual return
-          <input placeholder={percent_annual_return} onChange={ (e) => updateInputs(e.target.value, 'percent_annual_return') }></input>
+          Percentage annual return low
+          <input onChange={ (e) => updateInputs(e.target.value, 'percent_annual_return_low') }></input>
+        </div>
+        <div className='user-input'>
+          Percentage annual return high
+          <input onChange={ (e) => updateInputs(e.target.value, 'percent_annual_return_high') }></input>
         </div>
         <div className='user-input'>
           Percentage of salary you would like to contribute
-          <input placeholder={percent_to_contribute} onChange={ (e) => {updateInputs(e.target.value, 'percent_to_contribute'); calculateMonthlyContributions()} }></input>
+          <input onChange={ (e) => {updateInputs(e.target.value, 'percent_to_contribute'); calculateMonthlyContributions()} }></input>
         </div>
+
         <UserContributions id="1"/>
         { this.state.contributer_added ?
           <UserContributions id="2"/> :
@@ -58,6 +57,7 @@ class UserInput extends Component {
             Add contributer
           </div>
         }
+
         <div className='monthly-contribution'>
           Monthly Contribution: ${monthly_contribution}
         </div>
@@ -78,13 +78,6 @@ const mapDispatchToProps = dispatch => bindActionCreators({
 const mapStateToProps = state => {
   return {
     test: state.mainReducer.test,
-    current_age: state.mainReducer.current_age,
-    retirement_age: state.mainReducer.retirement_age,
-    stop_contribution_age: state.mainReducer.stop_contribution_age,
-    current_savings: state.mainReducer.current_savings,
-    monthly_contribution: state.mainReducer.monthly_contribution,
-    percent_annual_return: state.mainReducer.percent_annual_return,
-    percent_to_contribute: state.mainReducer.percent_to_contribute,
     monthly_contribution: state.mainReducer.monthly_contribution
   }
 }
--- src/reducers/main-reducer.js
+++ src/reducers/main-reducer.js
@@ -8,14 +8,32 @@ const initialState = {
   test: "We're almost there...",
 }
 
-const calculateMonthlyContributions = (salary1 = 0, match1 = 0, salary2 = 0, match2 = 0, percentToContribute = 0) => {
-  salary1 = parseInt(salary1)
-  salary2 = parseInt(salary2)
-  match1 = parseInt(match1)
-  match2 = parseInt(match2)
-  percentToContribute = parseInt(percentToContribute)
-  return Math.round((((salary1 + salary2) * (percentToContribute/100)) + match1 + match2)/12)
-}
+// const calculateMonthlyContributions = (salary1 = 0, match1 = 0, salary2 = 0, match2 = 0, percentToContribute = 0) => {
+//   return Math.round((((salary1 + salary2) * (percentToContribute/100)) + match1 + match2)/12)
+// }
+//
+// const calculateCompoundInterest = (current_age, retirement_age, stop_contribution_age, current_savings, monthly_contribution, percent_annual_return_low, percent_annual_return_high) => {
+//   let annualSavingsValues = []
+//   let currentSavingsValueLow = current_savings
+//   let currentSavingsValueHigh = current_savings
+//   let date = new Date().getFullYear()
+//
+//   let totalMonthsContributing = (stop_contribution_age - current_age) * 12
+//   for (let i = 1; i < totalMonthsContributing + 1; i++) {
+//
+//     currentSavingsValueLow += monthly_contribution + (currentSavingsValueLow * (percent_annual_return_low)/1200)
+//     currentSavingsValueLow = currentSavingsValueLow
+//
+//     currentSavingsValueHigh += monthly_contribution + (currentSavingsValueHigh * (percent_annual_return_high)/1200)
+//     currentSavingsValueHigh = currentSavingsValueHigh
+//
+//     if (i % 12 === 0) {
+//       date++
+//       annualSavingsValues.push({year: date, currentSavingsValueLow: Math.round(currentSavingsValueLow), currentSavingsValueHigh: Math.round(currentSavingsValueHigh)})
+//     }
+//   }
+//     return annualSavingsValues
+// }
 
 export default (state = initialState, action) => {
 
@@ -28,14 +46,21 @@ export default (state = initialState, action) => {
       }
 
     case UPDATE_INPUTS:
+
+      if (action.value === ""){
+        action.value = 0
+      }
+
       return {
         ...state,
-        [action.input_name]: action.value,
+        [action.input_name]: parseInt(action.value)
       }
 
     case CALCULATE_MONTHLY_CONTRIBUTIONS:
+
       let { annual_salary_1, annual_salary_2, employer_match_1, employer_match_2, percent_to_contribute } = state
       let monthly_contribution = calculateMonthlyContributions(annual_salary_1, employer_match_1, annual_salary_2, employer_match_2, percent_to_contribute)
+
       return {
         ...state,
         monthly_contribution: monthly_contribution
